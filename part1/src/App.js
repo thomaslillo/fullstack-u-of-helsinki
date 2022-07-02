de30dbe35a7e9b75804e0b7174d65ee5545d8030
thomaslilo <thomas.j.lilo@gmail.com>
2022-07-02 15:22:15 -0400

    learning about components and props

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,12 +5,50 @@ const App = () => {
   const now = new Date()
   const a = 10
   const b = 20
+  const username = 'Tom' 
 
+  // you can embed components in other components
   return (
     <div>
-      <p>Hello world</p>
-      <p>The time is {now.toLocaleTimeString()}</p>
-      <p>{a} plus {b} is {a + b}</p>
+      {/*passing props not required even if func has them*/}
+      <Header />
+      <Hello username={username} />
+      <Time now={now} />
+      <Addition a={a} b={b} />
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>Header of the page</h1>
+    </div>
+  )
+}
+
+function Time (props) {
+  return (
+    <div>
+      <p>The time is {props.now.toLocaleTimeString()}</p>
+    </div>
+  )
+}
+
+function Addition(props) {
+  return (
+    <div>
+      <p>{props.a} plus {props.b} is {props.a + props.b}</p>
+    </div>
+  )
+}
+
+// this can be used multiple times
+// props is used to data pass between components
+function Hello(props) {
+  return (
+    <div>
+      <p>Greetings {props.username}</p>
     </div>
   )
 }
